8bc1163b5ca7531c87c09a5d1bbaa6ba71422337
cadeniyi <chris.adeniyi-jones@arm.com>
2020-07-17 11:23:56 +0100

    add portmap for HOSTPORT support

--- build/smartercni.sh
+++ build/smartercni.sh
@@ -40,7 +40,7 @@ GW=${GW:="172.39.0.1"}
 NC=$(echo $CIDR | sed -e 's/\//\\\//')
      
 
-sed -e "s/CIDR/\"$NC\"/" /host/etc/cni/net.d/0-smarter-bridge.conf | sed -e "s/GW/\"$GW\"/" > /etc/cni/net.d/0-smarter-bridge.conf
+sed -e "s/CIDR/\"$NC\"/" /host/etc/cni/net.d/0-smarter-bridge.conflist | sed -e "s/GW/\"$GW\"/" > /etc/cni/net.d/0-smarter-bridge.conflist
 
 
 echo "Done"
